6ca130e46558a53eec7b9b289f35143d8eb288e3
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-21 17:19:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,50 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def Song.create
+    song = Song.new
+    song.save
+    song
+  end
+ 
+  def Song.new_by_name(song_name)
+    song = new
+    song.name = song_name
+    song
+  end
+  
+  def Song.create_by_name(song_name)
+    song = create
+    song.name = song_name
+    song
+  end
+  
+  def Song.find_by_name(song_name)
+      self.all.find{|title| title.name == song_name}
+  end
+  
+  def Song.find_or_create_by_name(song_name)
+    Song.find_by_name(song_name) || Song.create_by_name(song_name)
+  end
+  
+  def Song.alphabetical
+    self.all.sort_by{|title| title.name}
+  end
+  
+  def new_from_filename(filename)
+    
+  end
+  
+  def Song.create_from_filename
+    
+  end
+  
+  def Song.destroy_all
+    @@all = []
+  end
 end
+
+
+
+
